eb3a641849c372fc37f0df5f1b4a788e98ab2d2d
lmjvdl <lmjvdl82@gmail.com>
2025-07-26 00:20:39 +0330

    Edit and re-write next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
-  },
-  
+  }
 };
+
+export default nextConfig;
