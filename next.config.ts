4093047510ac424bcda02e6965741499a6e40bd0
lmjvdl <lmjvdl82@gmail.com>
2025-07-26 02:45:49 +0330

    Change next.config.ts file attributes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+  output: 'standalone',
   trailingSlash: true,
   webpack(config) {
     config.module.rules.push({
